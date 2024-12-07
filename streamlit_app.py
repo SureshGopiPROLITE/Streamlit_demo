1754fb97b7b050aedc764e1e2499c5fd2a4ce9ee
LSureshGopi <sgopi2672@gmail.com>
2024-12-07 13:29:22 +0530

    Initial add

--- streamlit_app.py
+++ streamlit_app.py
@@ -267,7 +267,7 @@ custom_css = """
         background-color: #FFFFFF;
         border: 2px solid #FF5733;
         border-radius: 10px;
-        padding: 10px;
+        padding: 0px;
         font-size: 1.2em;
     }
 
